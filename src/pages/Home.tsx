6d2dbe0e2744e6b9b52a6ca8e3d7d79635ebd1d4
Matthew Nadar <BOGUS@EMAIL>
2025-09-17 11:40:44 -0400

    adding some navbar functionality and styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import '../styles/Home.css'
 export default function Home() {
   return (
-    <div>
+    <div className="welcome-message">
       <h1>Welcome to the YChromes</h1>
       <p>UD’s premiere all male a cappella group </p>
+      <p>Click on some of the links above to learn more about us!</p>
     </div>
   );
 }
