5a4ea2ee7ac91f286b96e08db9968cbc5907bc64
Matthew Nadar <BOGUS@EMAIL>
2025-09-18 13:14:42 -0400

    Very basic about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,21 @@
+import '../styles/About.css'
+
 export default function About() {
-  return <h1>About the YChromes</h1>;
+  return (
+    <div className="About">
+      <h1>About the YChromes</h1>
+      <div className="About-card">
+        <p>
+          The YChromes are the University of Delaware’s premiere all-male a cappella group. 
+          Founded in 1994, we’ve been bringing high-energy performances, unique arrangements, 
+          and unforgettable harmonies to audiences on campus and beyond. 
+        </p>
+        <p>
+          From local showcases to national competitions, the YChromes combine musical excellence 
+          with personality, making every performance one to remember.
+        </p>
+    
+      </div>
+    </div>
+  )
 }
